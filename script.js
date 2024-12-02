a13d12c1a9337422a5178e1aabe470c7a33731d9
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-12-02 18:25:22 +0530

    completed

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Fetch news posts
+  const postsContainer = document.getElementById("news-posts");
+  if (postsContainer) {
+    fetch("posts.json")
+      .then((response) => response.json())
+      .then((posts) => {
+        posts.news.forEach((post) => {
+          const postElement = document.createElement("div");
+          postElement.innerHTML = `
+              <a href="news-post.html?id=${post.id}">
+                <div class="news-post">
+                  <img src="${post.img}" alt="${post.title}">
+                  <h1>${post.title}</h1>
+                  <p>${post.excerpt} 
+                    <a style="color: #4338ca;" href="news-post.html?id=${post.id}">Read more</a>
+                  </p>
+                  <small>Published on: ${post.date}</small>
+                </div>
+              </a>
+            `;
+          postsContainer.appendChild(postElement);
+        });
+      })
+      .catch((error) => console.error("Error fetching posts:", error));
+  }
+
+  // Fetch achievement posts
+  const achievementContainer = document.getElementById("achievement-posts");
+  if (achievementContainer) {
+    fetch("posts.json")
+      .then((response) => response.json())
+      .then((posts) => {
+        posts.achievements.forEach((post) => {
+          const postElement = document.createElement("div");
+          postElement.innerHTML = `
+              <a href="achievement-post.html?id=${post.id}">
+                <div class="achievement-post">
+                  <img src="${post.img}" alt="${post.title}">
+                  <h1>${post.title}</h1>
+                  <p>${post.excerpt.slice(0, 200)}<a style="color: #4338ca;" href="achievement-post.html?id=${post.id}"> Read more</a></p>
+                </div>
+              </a>
+            `;
+          achievementContainer.appendChild(postElement);
+        });
+      })
+      .catch((error) => console.error("Error fetching posts:", error));
+  }
+});
